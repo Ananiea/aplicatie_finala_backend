2478bc3858a3d4bb8b35d48f5ba5e7814ca45cc6
Ananiea <alexandru.ananiea@gmail.com>
2025-02-16 23:33:07 +0100

    Fix CORS policy

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(cors({
   origin: "*",
   methods: "GET,POST,OPTIONS",
   allowedHeaders: "Content-Type,Authorization"
-}));
+}))
+app.options("*", cors()); 
+;
 
 
 // Configurare conexiune PostgreSQL
